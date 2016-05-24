fb39797c8c357dbbb906d89ef055c886d11ef517
Charles Carper <ccarper03@gmail.com>
2016-05-24 19:21:13 -0400

    added a gameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class GameController : MonoBehaviour
+{
+    //Singleton Pattern
+    public static GameController instance;
+     
+    void Awake()
+    {
+        // If an instance already exists, destroy this one
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        // Otherwise, make this the instance
+        else
+        {
+            instance = this;
+            // Enable persistence across scenes
+            DontDestroyOnLoad(this.gameObject);
+        }
+       
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Keypad1))
+        {
+            SceneManager.LoadScene("Main");
+        }
+        if (Input.GetKeyUp(KeyCode.Keypad2))
+        {
+            SceneManager.LoadScene("SinglePlayer");
+        }
+    }
+}
